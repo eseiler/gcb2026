1262f3fc8626a538e8fa2dff92023cc1af4e46af
Enrico Seiler <enrico.seiler@hotmail.de>
2026-09-18 04:33:43 +0200

    fix: copyright

--- data/generate_mock_papers.sh
+++ data/generate_mock_papers.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 #
+# SPDX-FileCopyrightText: 2006-2026 Knut Reinert & Freie Universität Berlin
+# SPDX-FileCopyrightText: 2016-2026 Knut Reinert & MPI für molekulare Genetik
+# SPDX-License-Identifier: CC0-1.0
+#
 # generate_test_files.sh - Generate random "lorem ipsum" style text files for testing.
 #
 # Usage:
